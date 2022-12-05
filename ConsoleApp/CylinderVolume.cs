5d190dbcb823086904875c2ab1b5181a75d44064
user10 <work20go@mail.ru>
2022-12-05 16:10:01 +0200

    Add project files.

---
+++ ConsoleApp/CylinderVolume.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class CylinderVolume
+    {
+        private double Diameter;
+        private double Height;
+
+        public CylinderVolume(double diameter, double height)
+        {
+            Diameter = diameter;
+            Height = height;
+        }
+
+        public double getVolume()
+        {
+            return Math.PI * Diameter * Height;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите диаметр цииндра: ");
+            double diameter = double.Parse(Console.ReadLine());
+            Console.Write("Введите высоту цилиндра: ");
+            double height = double.Parse(Console.ReadLine());
+            CylinderVolume volume = new CylinderVolume(diameter, height);
+            Console.WriteLine($"Объём цилиндра равен {volume.getVolume()}");
+        }
+    }
+}
